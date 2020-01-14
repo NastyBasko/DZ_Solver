9162971863236a21dfe11e768f7eb58b907f3044
NastyBasko <59891056+NastyBasko@users.noreply.github.com>
2020-01-15 00:06:40 +0500

    Add files via upload

---
+++ Домашняя_работа/HomeWork/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace HomeWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.res = new System.Windows.Forms.TextBox();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.cost = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // res
+            // 
+            this.res.Location = new System.Drawing.Point(12, 257);
+            this.res.Multiline = true;
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(507, 276);
+            this.res.TabIndex = 0;
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(34, 209);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(100, 20);
+            this.x1.TabIndex = 1;
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(213, 209);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(100, 20);
+            this.x2.TabIndex = 2;
+            // 
+            // cost
+            // 
+            this.cost.Location = new System.Drawing.Point(400, 209);
+            this.cost.Name = "cost";
+            this.cost.Size = new System.Drawing.Size(100, 20);
+            this.cost.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Кол-во продукта 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Кол-во продукта 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(397, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Общая стоимость";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Лог событий";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Исходные данные";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(11, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(508, 150);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Ресурсы";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Продукт 1";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Продукт 2";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Объем ресурсов";
+            this.Column4.Name = "Column4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 540);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(437, 539);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 589);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cost);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.x1);
+            this.Controls.Add(this.res);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Составление плана производства с максимальным выпуском продукции";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox res;
+        private System.Windows.Forms.TextBox x1;
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.TextBox cost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Домашняя_работа/HomeWork/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SolverFoundation.Services;
+
+namespace HomeWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] names = {"Алюминий","Медь","Олово","Цинк","Свинец" };
+            int[] prod1 = {10,20,15,30,20};
+            int[] prod2 = {70,50,35,40,45};
+            int[] volume = {570,420,300,600,400};
+            dataGridView1.Rows.Add(); dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add(); dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            for (int i = 0; i <= 4; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = names[i];
+                dataGridView1.Rows[i].Cells[1].Value = prod1[i];
+                dataGridView1.Rows[i].Cells[2].Value = prod2[i];
+                dataGridView1.Rows[i].Cells[3].Value = volume[i];
+            }
+    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] names = { "Алюминий", "Медь", "Олово", "Цинк", "Свинец" };
+            int[] prod1 = { 10, 20, 15, 30, 20 };
+            int[] prod2 = { 70, 50, 35, 40, 45 };
+            int[] volume = { 570, 420, 300, 600, 400 };
+            dataGridView1.Rows.Add(); dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add(); dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            for (int i = 0; i <= 4; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = names[i];
+                dataGridView1.Rows[i].Cells[1].Value = prod1[i];
+                dataGridView1.Rows[i].Cells[2].Value = prod2[i];
+                dataGridView1.Rows[i].Cells[3].Value = volume[i];
+            }
+            SolverContext context = SolverContext.GetContext();
+            Model model = context.CreateModel();
+            Decision x01 = new Decision(Domain.IntegerNonnegative, "product1");
+            Decision x02 = new Decision(Domain.IntegerNonnegative, "product2");
+            model.AddDecisions(x01, x02);
+            model.AddConstraints("production",
+               0.4 * x01 - 0.6 * x02 >= 0,
+               x01 * prod1[0] + x02 * prod2[0] <= volume[0],
+                x01 * prod1[1] + x02 * prod2[1] <= volume[1],
+                x01 * prod1[2] + x02 * prod2[2] <= volume[2],
+                x01 * prod1[3] + x02 * prod2[3] <= volume[3],
+                x01 * prod1[4] + x02 * prod2[4] <= volume[4]
+            );
+            {
+                model.AddGoal("cost", GoalKind.Maximize, 30 * x01 + 80 * x02);
+                Solution solution = context.Solve(new SimplexDirective());
+                Report report = solution.GetReport();
+                x1.Text = x01.ToDouble().ToString();
+                x2.Text = x02.ToDouble().ToString();
+                cost.Text = model.Goals.First().ToDouble().ToString();
+                res.Text += report;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
